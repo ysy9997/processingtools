7a612f3078b8ad23b7a2c6a90507ba0cc58810fe
ysy999 <ysy9997@gmail.com>
2022-04-02 22:54:16 +0900

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ProcessingTools",
-    version="0.2.2",
+    version="0.2.3",
     install_requires=['opencv-python>=4.5.5.62',
                       'numpy>=1.19.4',
                       'matplotlib>=3.3.3'],
