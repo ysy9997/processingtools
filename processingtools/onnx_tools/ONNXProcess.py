fb663965b9f92801644ef5c9a82949948aa2d51e
ysy9997 <ysy9997@gmail.com>
2024-09-19 22:08:24 +0900

    update version and fix ONNXInferenceModel

--- processingtools/onnx_tools/ONNXProcess.py
+++ processingtools/onnx_tools/ONNXProcess.py
@@ -9,12 +9,13 @@ if typing.TYPE_CHECKING:
 
 
 class ONNXInferenceModel:
-    def __init__(self, onnx_model_path: str,
+    def __init__(self, onnx_model_path: str, size: typing.Union[tuple, list, None] = None,
                  mean: typing.Union[float, typing.Tuple[float, float, float], np.array] = np.array([0.0, 0.0, 0.0]),
                  std: typing.Union[float, typing.Tuple[float, float, float], np.array] = np.array([1.0, 1.0, 1.0])):
         """
         initialize
         :param onnx_model_path: onnx file path
+        :param size: size to which images will be resized
         :param mean: mean for normalization
         :param std: standard deviation for normalization
         """
@@ -24,6 +25,7 @@ class ONNXInferenceModel:
         self.ort_session = onnxruntime.InferenceSession(onnx_model_path)
         self.ort_input_name = self.ort_session.get_inputs()[0].name
 
+        self.size = size
         self.mean = mean
         self.std = std
 
@@ -47,14 +49,17 @@ class ONNXInferenceModel:
         :return: normalized image
         """
         try:
-            image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB).astype('float32') / 255.0
+            image = cv2.imread(image_path)
+            if self.size is not None:
+                image = cv2.resize(image, self.size)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) / 255.0
             image = np.transpose((image - self.mean) / self.std, (2, 0, 1))
-            return np.expand_dims(image, axis=0)
+            return np.expand_dims(image, axis=0).astype('float32')
         except Exception:
             raise Exception(f'Cannot read a image ({os.path.abspath(image_path)})!')
 
     @staticmethod
-    def to_numpy(tensor: torch.Tensor) -> np.ndarray:
+    def to_numpy(tensor: 'torch.Tensor') -> np.ndarray:
         """
         convert input tensor from torch tensor to numpy tensor
         :param tensor: torch input tensor
